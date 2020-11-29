23cfd4f909a46aa6a7491752ebb31f9011315f98
lucacambi@rentalcars.com <lucacambi@rentalcars.com>
2020-11-29 15:15:25 +0000

    adds basic auth to swagger

--- src/main/java/it/cambi/dhis2/swagger/SwaggerConfig.java
+++ src/main/java/it/cambi/dhis2/swagger/SwaggerConfig.java
@@ -5,9 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.BasicAuth;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.SecurityScheme;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,6 +25,24 @@ public class SwaggerConfig {
         .select()
         .apis(RequestHandlerSelectors.basePackage(Dhis2Controller.class.getPackageName()))
         .paths(PathSelectors.any())
+        .build()
+        .securityContexts(Collections.singletonList(securityContext()))
+        .securitySchemes(Collections.singletonList(basicAuthScheme()));
+  }
+
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(Collections.singletonList(basicAuthReference()))
+        .operationSelector(
+            operationContext -> operationContext.requestMappingPattern().contains("/dhis2/v1/api/"))
         .build();
+  }
+
+  private SecurityScheme basicAuthScheme() {
+    return new BasicAuth("basicAuth");
+  }
+
+  private SecurityReference basicAuthReference() {
+    return new SecurityReference("basicAuth", new AuthorizationScope[0]);
   }
 }
